df5680464d4ddbcf7949114ed2295c30dd13d6e8
MichaelElliott27 <83131933+MichaelElliott27@users.noreply.github.com>
2021-08-19 11:34:19 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,31 @@
+const accessCode1 = "CLASS";
+const accessCode2 = "JAVA SCRIPT";
+const accessCode3 = "FIREBASE";
+const accessCode4 = "SPRITES";
+function clues() {
+    
+    fill("red")
+    textSize(15)
+    text("A S C L S", 100,90)
+    fill("red")
+    text("Hint: Similar to a blueprint !!", 100,110)
+
+    fill("orange")
+    textSize(15)
+    text("A V J A  C R I S T P ", 700,150)
+    fill("orange")
+    text("Hint: A language of Coding !!", 700,170)
+
+    fill("yellow")
+    textSize(15)
+    text("A S E F R I B E", 100,290)
+    fill("yellow")
+    text("Hint: Realtime database we use !!", 100,310)
+
+    fill("orange")
+    textSize(15)
+    text("E S R P T S I", 700,290)
+    fill("yellow")
+    text("Hint: draw ____ !!", 700,310)
+
+}
